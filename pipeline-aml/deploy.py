916adad945442c73e3c55fda4c86bc20e322235b
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-05-10 13:52:24 -0500

    pipeline AML

---
+++ pipeline-aml/deploy.py
@@ -0,0 +1,77 @@
+import os
+import azureml.core
+from azureml.core import Workspace, Experiment, Dataset, RunConfiguration
+from azureml.pipeline.core import Pipeline, PipelineData
+from azureml.pipeline.steps import PythonScriptStep
+from azureml.data.dataset_consumption_config import DatasetConsumptionConfig
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
+from azureml.core import Dataset
+from azureml.pipeline.core import PipelineEndpoint
+
+# Verificamos version del Azure ML SDK
+print("Azure ML SDK version:", azureml.core.VERSION)
+
+# Cargamos el workspace de AML
+ws = Workspace.from_config()
+print(f'WS name: {ws.name}\nRegion: {ws.location}\nSubscription id: {ws.subscription_id}\nResource group: {ws.resource_group}')
+
+# Creacion del compute cluster si es que no existe
+aml_compute_target = "cpu-cluster"
+try:
+    aml_compute = AmlCompute(ws, aml_compute_target)
+except ComputeTargetException:
+    config = AmlCompute.provisioning_configuration(vm_size = "STANDARD_D2_V2", min_nodes = 0, max_nodes = 1,
+                                                   idle_seconds_before_scaledown=3600)
+    aml_compute = ComputeTarget.create(ws, aml_compute_target, config)
+    aml_compute.wait_for_completion(show_output=True, min_node_count=None, timeout_in_minutes=20)
+
+# Obtenemos el dataset y lo subimos al worskpace
+datastore = ws.get_default_datastore()
+datastore.upload(src_dir='../data-training', target_path='german-credit-train-tutorial', overwrite=True)
+ds = Dataset.File.from_files(path=[(datastore, 'german-credit-train-tutorial')])
+ds.register(ws, name='german-credit-train-tutorial', description='Dataset for workshop tutorials', create_new_version=True)
+
+# Algo del dataset que aun no se que será
+training_dataset = Dataset.get_by_name(ws, "german-credit-train-tutorial")
+# Download dataset to compute node - we can also use .as_mount() if the dataset does not fit the machine
+training_dataset_consumption = DatasetConsumptionConfig("training_dataset", training_dataset).as_download()
+
+
+# Configuracion del pipeline
+runconfig = RunConfiguration.load("runconfig.yml")
+
+train_step = PythonScriptStep(name="train-step",
+                        source_directory="./",
+                        script_name="train.py",
+                        arguments=['--data-path', training_dataset_consumption],
+                        inputs=[training_dataset_consumption],
+                        runconfig=runconfig,
+                        allow_reuse=False)
+
+steps = [train_step]
+
+# Creamos el objeto de pipeline y lo validamos
+pipeline = Pipeline(workspace=ws, steps=steps)
+pipeline.validate()
+
+# Enviar el pipeline frente a un experimento
+pipeline_run = Experiment(ws, 'training-pipeline').submit(pipeline)
+pipeline_run.wait_for_completion()
+
+# Publicamos el pipeline
+published_pipeline = pipeline.publish('training-pipeline')
+published_pipeline
+
+
+# Publicar el pipeline como endpoint
+endpoint_name = "training-pipeline-endpoint"
+try:
+   pipeline_endpoint = PipelineEndpoint.get(workspace=ws, name=endpoint_name)
+   # Add new default endpoint - only works from PublishedPipeline
+   pipeline_endpoint.add_default(published_pipeline)
+except Exception:
+    pipeline_endpoint = PipelineEndpoint.publish(workspace=ws,
+                                            name=endpoint_name,
+                                            pipeline=pipeline,
+                                            description="New Training Pipeline Endpoint")
---
+++ pipeline-aml/main/train.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import pickle
+import argparse
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from azureml.core import Run
+
+def get_runtime_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-path', type=str)
+    args = parser.parse_args()
+    return args
+
+def feature_engineer_data(dataset_path):
+    dataset = pd.read_csv(dataset_path)
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, 1].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
+    return X_train, X_test, y_train, y_test
+
+def train_model(X_train, y_train):
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+    return regressor
+
+def predict(regressor, X_test):
+    y_pred = regressor.predict(X_test)
+    return y_pred
+
+def export_model(regressor, model_path):
+    pickle.dump(regressor, open(model_path,'wb'))
+
+def import_model_predict(model_path, value, debug=False):
+    """
+        pickle.load() method loads the method and saves the deserialized bytes to model.
+        Predictions can be done using model.predict().
+        load again
+    """
+    model = pickle.load(open(model_path,'rb'))
+    output = model.predict(value)
+    if debug:
+        print(output)
+    return output
+
+def execute_pipeline():
+    args = get_runtime_args()
+    # step 1
+    dataset_path = os.path.join(args.data_path, 'compensation_dataset.csv')
+    X_train, X_test, y_train, y_test = feature_engineer_data(dataset_path)
+
+    # step 2
+    model = train_model(X_train, y_train)
+
+    # step 3
+    y_test_results = predict(model, X_test)
+    run = Run.get_context()
+    run.log('Resultados Y-Test', y_test_results)
+
+# run the code
+if __name__ == '__main__':
+    execute_pipeline()
+
